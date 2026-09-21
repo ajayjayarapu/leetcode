41a5a4e59f8b221f372a1540cc6a440eb12f1c68
ajayjayarapu <ajay.jayavarapu@gmail.com>
2026-09-21 17:23:40 +0530

    project structure change

--- src/main/java/com/arraysandhashing/ContainsDuplicate.java
+++ src/main/java/com/arraysandhashing/ContainsDuplicate.java
@@ -1,9 +1,11 @@
-
+package src.main.java.com.arraysandhashing;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
+
+
 
     public static void main(String[] args) {
         int[] nums = {1,5,-2,-4,0};
